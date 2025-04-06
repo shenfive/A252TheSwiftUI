64226b15594e272432005fb4c30b60e59460cc70
Danny Shen <shenfive@gmail.com>
2025-04-06 11:39:08 +0800

    CardView

---
+++ A252TheSwiftUI/CardView.swift
@@ -0,0 +1,36 @@
+//
+//  CardView.swift
+//  A252TheSwiftUI
+//
+//  Created by 申潤五 on 2025/4/6.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    var content = "👌"
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(
+                cornerRadius: 20.0)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            }else{
+                shape.fill()
+            }
+        }
+        .onTapGesture{
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+#Preview {
+    CardView(content: "Hello")
+}
--- A252TheSwiftUI/ContentView.swift
+++ A252TheSwiftUI/ContentView.swift
@@ -6,43 +6,14 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
-  
+    var emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"]
     
-    @State var r = 0.5
-    @State var g = 0.5
-    @State var b = 0.5
     
     var body: some View {
-        ZStack{
-            theBackgroud
-            VStack{
-                HStack{
-                    Text("R: \(String(format: "%3.0f", r * 255))")
-                    Text("G: \(String(format: "%3.0f", g * 255))")
-                    Text("B: \(String(format: "%3.0f", b * 255))")
-                }
-                .padding(20)
-                .font(.largeTitle)
-                Spacer()
-                Slider(value: $r)
-                    .padding(.horizontal, 30.0)
-                    .padding(.vertical,10.0)
-                Slider(value: $g)
-                    .padding(.horizontal, 30.0)
-                    .padding(.vertical,10.0)
-                Slider(value: $b)
-                    .padding(.horizontal, 30.0)
-                    .padding(.top,10.0)
-                    .padding(.bottom,30.0)
-            }
-        }
+        CardView(content:"😍")
     }
     
-    var theBackgroud:some View {
-        Color(red: r, green: g, blue: b)
-    }
     
 }
 
