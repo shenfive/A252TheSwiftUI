c59250f89e58e7d9b9a0f8361f1bc90a62f14615
Danny Shen <shenfive@gmail.com>
2025-04-06 11:05:30 +0800

    調色盤

--- A252TheSwiftUI/ContentView.swift
+++ A252TheSwiftUI/ContentView.swift
@@ -18,26 +18,24 @@ struct ContentView: View {
         ZStack{
             theBackgroud
             VStack{
-                Text("test")
+                HStack{
+                    Text("R: \(String(format: "%3.0f", r * 255))")
+                    Text("G: \(String(format: "%3.0f", g * 255))")
+                    Text("B: \(String(format: "%3.0f", b * 255))")
+                }
+                .padding(20)
+                .font(.largeTitle)
                 Spacer()
                 Slider(value: $r)
                     .padding(.horizontal, 30.0)
-                    .padding(
-                        .vertical,
-                        10.0
-                    )
+                    .padding(.vertical,10.0)
                 Slider(value: $g)
                     .padding(.horizontal, 30.0)
-                    .padding(
-                        .vertical,
-                        10.0
-                    )
+                    .padding(.vertical,10.0)
                 Slider(value: $b)
                     .padding(.horizontal, 30.0)
-                    .padding(
-                        .vertical,
-                        10.0
-                    )
+                    .padding(.top,10.0)
+                    .padding(.bottom,30.0)
             }
         }
     }
